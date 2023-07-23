29a8b19c2e25f55a3c3b7c27240a1a32a942dcd3
Martin Blagoev <mb.reppair@gmail.com>
2023-07-24 02:36:33 +0300

    Fix the console kernel schedule.

--- api/app/Console/Kernel.php
+++ api/app/Console/Kernel.php
@@ -16,7 +16,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command(FetchUserWeather::class)->everyMinute();
+        $schedule->job(FetchUserWeather::class)->everyTenMinutes();
     }
 
     /**
