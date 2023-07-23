b580e5ea66f48aa06b1236767588226f6bd03bb8
Martin Blagoev <mb.reppair@gmail.com>
2023-07-24 02:40:11 +0300

    fix some typos in my comments

--- api/app/Services/Weather/WeatherData.php
+++ api/app/Services/Weather/WeatherData.php
@@ -6,20 +6,19 @@ class WeatherData
 {
     /**
      * A simple DTO? Sure, why not.
-     *
-     * Current doesn't make much sense, it is here just to illustrate that it could be useful if we had more detailed feature spec.
+     * Current version doesn't make much sense, it is here just to illustrate that it could be useful.
      */
     public function __construct(
         public readonly string $latitude,
         public readonly string $longitude,
         public readonly string $timezone,
     ) {
-        // self check if the passed data is valid here...
+        // todo: self check if the passed data is valid here...
     }
 
     public function isValid(): bool
     {
-        // validation goodness here...
+        // todo: validation goodness here...
 
         return true;
     }
